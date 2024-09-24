547afc97ce64b7e885ba8758ab6f567e5789c36f
Mohammed Maghri <mohammedmaghri3301@gmail.com>
2024-09-24 20:36:16 +0200

    challenge6

---
+++ Day01/Variables/challenge6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+   float a, b;
+   printf("enter number A :");
+   scanf("%f",&a);
+   printf("enter number B :");
+   scanf("%f",&b);
+   printf("A + B = %.2f\n",a+b);
+   printf("A - B = %.2f\n",a-b);
+   printf("A * B = %.2f\n",a*b);
+   printf("A / B = %.2f\n",a/b);
+
+
+    return 0;
+
+}
