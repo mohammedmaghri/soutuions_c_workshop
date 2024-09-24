dd1e230db0f9ff89501613ab253db47b399d7c29
Mohammed Maghri <mohammedmaghri3301@gmail.com>
2024-09-24 19:40:06 +0200

    challenge3

---
+++ Day01/Variables/challenge3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float distance,Yards;
+    printf("enter la distance :");
+    scanf("%f",&distance);
+    Yards = distance * 1093.61;
+
+    printf("Yards = %.2f",Yards);
+
+    return 0;
+}
