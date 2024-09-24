8f886bad2237d59a7816ef4fecb32a20a85d8181
Mohammed Maghri <mohammedmaghri3301@gmail.com>
2024-09-24 17:27:25 +0200

    challenge1

---
+++ Day01/Variables/Challenge1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int age;
+    char name[20];
+    char prenom[20];
+    char email[50];
+
+   printf("enter first name :");
+   scanf("%s",name);
+   printf("enter second name :");
+   scanf("%s",prenom);
+   printf("what is your age :");
+   scanf("%d",&age);
+   printf("enter your email :");
+   scanf("%s",email);
+
+
+    return 0;
+}
